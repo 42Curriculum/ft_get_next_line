6113d7e352fcd5cf0ac35b9d21e30deade223db1
Johanna Josephine <mahthaki@gmail.com>
2019-10-03 17:24:06 -0700

    leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jjosephi <jjosephi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 16:49:46 by jjosephi          #+#    #+#             */
-/*   Updated: 2019/09/30 13:24:16 by jjosephi         ###   ########.fr       */
+/*   Updated: 2019/10/03 17:23:41 by jjosephi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,53 +28,60 @@ int		check_line(char *str)
 	return (FALSE);
 }
 
-void	skip_n(char **str)
-{
-	while (**str == '\n')
-		*str += 1;
-}
 
-char	*cpy_nn(char *line, char **next_line)
+char	*cpy_nn(char **line, char **next_line)
 {
 	int i;
-
+	char **temp;
+	
 	i = 0;
+	temp = next_line;
 	while ((*next_line)[i] != '\n' && (*next_line)[i] != '\0')
 		i++;
-	line = ft_strnew(i);
-	line = ft_strncpy(line, *next_line, i);
-	*next_line += i;
-	return (line);
+	*line = ft_strnew(i);
+	*line = ft_strncpy(*line, *next_line, i);
+	*next_line = ft_strdup(*next_line + (i + 1));
+	free(*temp);
+	//*next_line += i + 1;
+	return (*line);
 }
 
 int		get_next_line(const int fd, char **line)
 {
 	char		buffer[BUFF_SIZE + 1];
-	static char **next_line;
+	static char *next_line[FD_NOTMAX];
 	int			b;
 
-	if (!(next_line))
-		next_line = malloc(4094 * (sizeof(next_line) + 1));
 	if (!(next_line[fd]))
-		next_line[fd] = "";
-	skip_n(&next_line[fd]);
-	if ((check_line(next_line[fd]) == TRUE))
+		next_line[fd] = ft_strnew(0);
+	if (fd < 0)
+		return (-1);
+	if ((check_line(next_line[fd])))
 	{
 		if (next_line[fd][0] == '\0')
 			return (0);
-		*line = cpy_nn(*line, &next_line[fd]);
+		*line = cpy_nn(line, &next_line[fd]);
 		return (1);
 	}
+
 	while ((b = read(fd, buffer, BUFF_SIZE)))
 	{
 		buffer[b] = '\0';
 		next_line[fd] = ft_strjoin(next_line[fd], buffer);
-		skip_n(&next_line[fd]);
-		if ((check_line(next_line[fd]) == TRUE))
+		if ((check_line(next_line[fd])))
 		{
-			*line = cpy_nn(*line, &next_line[fd]);
+			*line = cpy_nn(line, &next_line[fd]);
+			ft_bzero(buffer, BUFF_SIZE);
 			return (1);
 		}
 	}
+	if (ft_strlen(next_line[fd]) > 0)
+	{
+		*line = ft_strdup(next_line[fd]);
+		ft_strdel(&next_line[fd]);
+		next_line[fd] = NULL;
+		return (1);
+	}
+
 	return (0);
 }
--- next_line_test.c
+++ next_line_test.c
@@ -12,14 +12,8 @@ int		main(int argc, char **argv)
 	i = open(argv[1], O_RDONLY);
 	if (argc > 1)
 	{
-		if (get_next_line(i, &str) == 1)
+		while (get_next_line(i, &str) == 1)
 			printf("Line 1: %s\n", str);
-		if (get_next_line(i, &str) == 1)
-			printf("Line 2: %s\n", str);
-		if (get_next_line(i, &str) == 1)
-			printf("Line 3: %s\n", str);
-		if (get_next_line(i, &str) == 1)
-			printf("Line 4: %s\n", str);
 	}
 	return (0);
 }
